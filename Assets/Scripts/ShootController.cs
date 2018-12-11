141b89c9138268f9e9ab87fbd6bfa10bad2aa9f4
PPeMg <jdeldo@gmail.com>
2018-12-11 19:39:45 +0100

    Added Bolts. Coded movement.

---
+++ Assets/Scripts/ShootController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rig;
+	
+    void Awake () {
+        rig = GetComponent<Rigidbody>();
+	}
+
+    void Start()
+    {
+        rig.velocity = transform.forward * speed;
+    }
+}
