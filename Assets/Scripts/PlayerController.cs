af6b21c9abdd2d44315502186bf4c29e3197da09
PPeMg <jdeldo@gmail.com>
2018-12-11 20:39:18 +0100

    Added Shoot functionality to player.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,17 +10,37 @@ public class Boundary
 
 public class PlayerController : MonoBehaviour {
 
+    [Header("Movement")]
     public float speed;
     public float tilt;
     public Boundary boundary;
 
     private Rigidbody rig;
 
+    [Header("Shoot")]
+    public GameObject shoot;
+    public float fireRate;
+
+    private float nextFire;
+    private Transform shootSpawn;
+
     void Awake () {
         rig = GetComponent<Rigidbody>();
+
+        shootSpawn = GameObject.Find("Shoot Spawn").GetComponent<Transform>();
+        nextFire = 0f;
 	}
 
-	void FixedUpdate () {
+    private void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shoot, shootSpawn.position, Quaternion.identity);
+        }
+    }
+
+    void FixedUpdate () {
         float speedHorizontal = Input.GetAxis("Horizontal") * speed;
         float speedVertical = Input.GetAxis("Vertical") * speed;
         float speedRotation = Input.GetAxis("Horizontal");
