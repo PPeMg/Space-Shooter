5b1742cde1791894053166f9b40a422b79a887e6
PPeMg <jdeldo@gmail.com>
2018-12-12 16:55:42 +0100

    Created Asteroid Prefab. Added Explosions

--- Assets/Scripts/BasicMovementController.cs
+++ Assets/Scripts/BasicMovementController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShootController : MonoBehaviour {
+public class BasicMovementController : MonoBehaviour {
 
     public float speed;
 
--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+        
     void OnTriggerEnter(Collider other)
     {
         if(!other.CompareTag("Boundary"))
         {
+            Instantiate(explosion, this.transform.position, this.transform.rotation);
+            if (other.CompareTag("Player"))
+            {
+                Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            }
             Destroy(other.gameObject);
             Destroy(this.gameObject);
         }
