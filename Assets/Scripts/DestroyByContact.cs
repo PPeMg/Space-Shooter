2bc541f79dc3f6dffd3d9bb31017cc5f6ece1be1
PPeMg <jdeldo@gmail.com>
2018-12-12 16:39:38 +0100

    Added Asteroid and First Functionality

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+    void OnTriggerEnter(Collider other)
+    {
+        if(!other.CompareTag("Boundary"))
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+
+    public float tumble;
+
+    private Rigidbody rig;
+
+	void Awake () {
+        rig = GetComponent<Rigidbody>();
+	}
+
+    private void Start()
+    {
+        //rig.angularVelocity = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1)).normalized;
+        rig.angularVelocity = Random.insideUnitSphere;
+        rig.angularVelocity *= tumble;
+    }
+}
