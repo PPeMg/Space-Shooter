11e2dd641a322f3671b79d1d747cb52f7c9f1416
PPeMg <jdeldo@gmail.com>
2018-12-17 19:29:00 +0100

    Game Finished. Added Evasive Movements to Enemy Ships

---
+++ Assets/Scripts/EvasiveActions.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvasiveActions : MonoBehaviour {
+
+    public float dodge;
+    public float smoothAction;
+    public float tilt;
+    public Vector2 waitInterval;
+    public Vector2 duration;
+
+    private Vector2 viewportWidth;
+    private float targetSpeed;
+    private Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        UpdateBoundarys();
+        StartCoroutine(Evade());
+    }
+
+    private void UpdateBoundarys()
+    {
+        Vector2 halfDimensions = Utils.GetDimensionsInWorldUnits() / 2;
+
+
+        this.viewportWidth.x = -halfDimensions.x + 0.7f;
+        this.viewportWidth.y = halfDimensions.x - 0.7f;
+    }
+
+    IEnumerator Evade()
+    {
+        do
+        {
+            yield return new WaitForSeconds(Random.Range(waitInterval.x, waitInterval.y));
+
+            targetSpeed = Random.Range(1, dodge) * -Mathf.Sign(transform.position.x);
+
+            yield return new WaitForSeconds(Random.Range(duration.x, duration.y));
+
+            targetSpeed = 0;
+        } while (true);
+    }
+
+    void FixedUpdate () {
+        float newSpeed = Mathf.MoveTowards(rb.velocity.x, targetSpeed, Time.deltaTime * smoothAction);
+        rb.velocity = new Vector3(newSpeed, 0f, rb.velocity.z);
+        rb.position = new Vector3(Mathf.Clamp(rb.position.x, viewportWidth.x, viewportWidth.y), 0, rb.position.z);
+
+        rb.rotation = Quaternion.Euler(0f, 0f, -tilt * newSpeed);
+    }
+}
